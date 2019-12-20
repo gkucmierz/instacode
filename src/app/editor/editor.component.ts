c5ef2442b3a730aa953afdbcd8e070b2a1d1a6ac
Grzegorz Kucmierz <gkucmierz@gmail.com>
2019-12-20 09:32:29 +0100

    fixed styling & code coloring

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,6 +1,8 @@
 import { OnInit, Component, ViewChild } from '@angular/core';
 import { CodemirrorComponent } from '@ctrl/ngx-codemirror';
-// import 'codemirror/mode/javascript/javascript';
+
+import 'codemirror/mode/javascript/javascript';
+// import 'codemirror/mode/markdown/markdown';
 
 @Component({
   selector: 'app-editor',
@@ -8,30 +10,44 @@ import { CodemirrorComponent } from '@ctrl/ngx-codemirror';
   styleUrls: ['./editor.component.scss'],
 })
 export class EditorComponent {
-  @ViewChild('codeEditor', { static: false }) codeEditor: CodemirrorComponent;
+  @ViewChild('codeEditor', { static: true }) codeEditor: CodemirrorComponent;
 
   private sourceCode = `
+// https://gist.github.com/gkucmierz/97a36a8ccdb37557cac6af847745e3be
+// play sine wave in browser
 
-import { OnInit, Component, ViewChild } from '@angular/core';
-import { CodemirrorComponent } from '@ctrl/ngx-codemirror';
-import 'codemirror/mode/javascript/javascript';
+const sound = (() => {
+  return {
+    play: (duration = 1e3) => {
+      const context = new AudioContext();
+      const o = context.createOscillator();
+      o.type = 'sine';
+      o.connect(context.destination);
+      o.start();
+      setTimeout(() => o.stop(), duration);
+    }
+  };
+})();
 
-
-@Component({
-  selector: 'app-editor',
-  templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss'],
-})
-  `;
+setInterval(sound.play, 2e3);
+`;
   
   readonly codemirrorOptions = {
-    lineNumbers: false,
-    theme: 'material',
+    value: 'dupa',
+    theme: 'monokai',
     autofocus: true,
-    // viewportMargin: Infinity,
-    mode: {
-      name: 'javascript',
-      typescript: true,
-    }
+    viewportMargin: Infinity,
+    indentUnit: 2,
+    smartIndent: true,
+    tabSize: 4,
+    indentWithTabs: false,
+    lineWrapping: false,
+    lineNumbers: true,
+    lineNumberFormatter: ln => `${ln}`,
+    fixedGutter: true,
+    scrollbarStyle: 'null',
+    mode: 'text/typescript',
+    // mode: 'markdown',
+    // mode: 'htmlmixed',
   };
 }
