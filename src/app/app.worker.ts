58215245835192c7fec907f3a5356b5f7925974b
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-04-28 22:21:11 +0200

    fix circleci build

--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -26,25 +26,17 @@ const throttledPM = (() => {
   let dataCache = [];
   let lastUpdate = +new Date() - updateDelay;
 
-<<<<<<< HEAD
-  return (data, finish = false) => {
-    const now = +new Date();
-    dataCache.push(data);
-=======
   return (data, finish = false, tryAgain = true) => {
     const now = +new Date();
     if (typeof data !== 'undefined') {
       dataCache.push(data);
     }
->>>>>>> web-app
 
     if (lastUpdate + updateDelay <= now || finish) {
       pm(limitData(dataCache.join('\n')));
       lastUpdate = now;
       dataCache = [];
     }
-<<<<<<< HEAD
-=======
 
     if (tryAgain) {
       const nextTick = fn => setTimeout(fn, 0);
@@ -54,7 +46,6 @@ const throttledPM = (() => {
       // try push possibly cached data for async code
       setTimeout(tryAgainFn, updateDelay);
     }
->>>>>>> web-app
   };
 })();
 
@@ -72,12 +63,6 @@ addEventListener('message', ({ data }) => {
   try {
     const code = new Function(data);
     code();
-<<<<<<< HEAD
-    const nextTick = fn => setTimeout(fn, 0);
-    // push data, that possibly waits (for sync code)
-    nextTick(_ => throttledPM('', true));
-=======
->>>>>>> web-app
   } catch (e) {
     console.error(e);
   }
